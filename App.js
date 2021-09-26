65bce353fad5f8935cfc1d8f7689b9906c8c4efa
Yamazaki Norihito <kizinn1224@gmail.com>
2021-09-26 14:14:41 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.itemContainer}></View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	itemContainer: {
+		height: 100,
+		width: "100%",
+		borderColor: "gray",
+		borderWidth: 1,
+	},
+});
