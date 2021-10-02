230dddee4c58dc178de9d63287f36a5a44a55a22
Yamazaki Norihito <kizinn1224@gmail.com>
2021-10-02 23:07:14 +0900

    news list coponent add

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
+import ListItem from "./components/ListItem";
+import articles from "./dummies/articles";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<View style={styles.itemContainer}>
-				<View style={styles.leftContainer}>
-					<Image
-						style={{ width: 100, height: 100 }}
-						source={{ uri: "https://unsplash.it/630/400" }}
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				data={articles}
+				renderItem={({ item }) => (
+					<ListItem
+						imageUrl={item.urlToImage}
+						title={item.title}
+						author={item.author}
 					/>
-				</View>
-				<View style={styles.rightContainer}>
-					<Text numberOfLines={3} style={styles.text}>
-						田村憲久厚生労働相は26日のNHK番組で、今月30日を期限とする新型コロナウイルス対応の緊急事態宣言の解除について「この状況でいけば実現できると思っている」と述べ、改めて可能性が高いと強調した。
-					</Text>
-					<Text style={styles.subText}>React News</Text>
-				</View>
-			</View>
-		</View>
+				)}
+				keyExtractor={(item, index) => index.toString()}
+			/>
+		</SafeAreaView>
 	);
 }
 
@@ -27,30 +25,5 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	itemContainer: {
-		height: 100,
-		width: "100%",
-		borderColor: "gray",
-		borderWidth: 1,
-		flexDirection: "row",
-	},
-	leftContainer: {
-		width: 100,
-	},
-	rightContainer: {
-		flex: 1,
-		flexDirection: "column",
-		padding: 10,
-		justifyContent: "space-between",
-	},
-	text: {
-		fontSize: 16,
-	},
-	subText: {
-		fontSize: 12,
-		color: "gray",
 	},
 });
---
+++ readme.md
@@ -0,0 +1,36 @@
+- create app
+
+```bash
+$ expo init news-app
+? Choose a template: › - Use arrow-keys. Return to submit.
+    ----- Managed workflow -----
+❯   blank               a minimal app as clean as an empty canvas    # ←chooise
+    blank (TypeScript)  same as blank but with TypeScript configuration
+    tabs (TypeScript)   several example screens and tabs using react-navigation and TypeScript
+    ----- Bare workflow -----
+    minimal             bare and minimal, just the essentials to get you started
+
+```
+
+- expo login
+
+```bash
+$ expo login
+
+```
+
+- start
+  https://docs.expo.dev/workflow/expo-cli/#expo-start
+
+```bash
+$ expo start -w   # ブラウザ
+$ expo start -i   # iphone
+$ expo start -a   # android
+$ expo start -c   # キャッシュクリア
+
+```
+
+- 実機で動かす場合、Expo Go を Install 必須(https://expo.dev/client)
+- Expo を使ったプロジェクトから Expo を使わないプロジェクトにする場合、"eject" で変換する（逆はできない
+
+
