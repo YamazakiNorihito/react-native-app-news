2c09e0d3452e42897b94c0e57d97a2b3220f4b44
Yamazaki Norihito <kizinn1224@gmail.com>
2021-10-03 14:20:06 +0900

    news api 実装

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-import React from "react";
-import { StyleSheet, View, FlatList, SafeAreaView } from "react-native";
-import ListItem from "./components/ListItem";
-import articles from "./dummies/articles";
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import axios from 'axios';
+import Constants from 'expo-constants';
+import ListItem from './components/ListItem';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 export default function App() {
-	return (
-		<SafeAreaView style={styles.container}>
-			<FlatList
-				data={articles}
-				renderItem={({ item }) => (
-					<ListItem
-						imageUrl={item.urlToImage}
-						title={item.title}
-						author={item.author}
-					/>
-				)}
-				keyExtractor={(item, index) => index.toString()}
-			/>
-		</SafeAreaView>
-	);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 });
