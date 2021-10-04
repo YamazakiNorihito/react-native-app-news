bb5ae00dd28be1847ecb8c709c64a2dfa3d25d48
Yamazaki Norihito <kizinn1224@gmail.com>
2021-10-04 22:26:17 +0900

    redux persist local storage save

--- store/index.js
+++ store/index.js
@@ -1,11 +1,23 @@
-import { createStore, combineReducers } from 'redux';
-import userReducer from './reducers/user';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import Storage from "react-native-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistReducer, persistStore } from "redux-persist";
+import userReducer from "./reducers/user";
+
+const persistConfig = {
+	key: "root",
+	storage: AsyncStorage,
+	whitelist: ["user"],
+};
 
 const rootReducer = combineReducers({
-  user: userReducer,
+	user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
